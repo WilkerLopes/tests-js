757d1dbe3ce14000fd0e36107be480f72f2850e7
Wilker Lopes <wilkerlopes257@gmail.com>
2022-04-20 15:57:15 -0400

    add throw erros

--- src/services/calculator.js
+++ src/services/calculator.js
@@ -1,3 +1,8 @@
 module.exports.sum = (num1, num2) => {
-    return num1 + num2;
+    var aux1 = parseInt(num1),
+        aux2 = parseInt(num2);
+
+    if (Number.isNaN(aux1) || Number.isNaN(aux2)) throw new Error("Valores inválidos, verifique se os valores passados estão coprretos.");
+
+    return aux1 + aux2;
 };
--- src/tests/calculator.spec.js
+++ src/tests/calculator.spec.js
@@ -3,3 +3,25 @@ const { sum } = require("../services/calculator");
 it("testa soma de 2 + 2 = 4", () => {
     expect(sum(2, 2)).toBe(4);
 });
+
+it("testa soma de 2 + + = 4, validadno se caso for ppassado uma string", () => {
+    expect(sum("2", "2")).toBe(4);
+});
+
+it("testa o validador de erros caso seja passado valores inválidos ", () => {
+    expect(() => {
+        sum("", 2);
+    }).toThrowError();
+
+    expect(() => {
+        sum([2, 2]);
+    }).toThrowError();
+
+    expect(() => {
+        sum({});
+    }).toThrowError();
+
+    expect(() => {
+        sum();
+    }).toThrowError();
+});
